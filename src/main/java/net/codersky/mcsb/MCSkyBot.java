a0aef0a4742b038df7d799674a31d161b9d03511
xDec0de_ <xdec0deyt@gmail.com>
2025-04-10 14:25:30 +0200

    Added config setup

--- src/main/java/net/codersky/mcsb/MCSkyBot.java
+++ src/main/java/net/codersky/mcsb/MCSkyBot.java
@@ -1,8 +1,56 @@
 package net.codersky.mcsb;
 
-public class MCSkyBot {
+import net.codersky.jsky.cli.CLICommandManager;
+import net.codersky.jsky.yaml.YamlFile;
+import org.jetbrains.annotations.NotNull;
 
-	public static void main(String[] args) {
-		
+import java.io.File;
+import java.util.function.Predicate;
+
+public abstract class MCSkyBot {
+
+	private final File dataFolder;
+	private final YamlFile cfg;
+	private final CLICommandManager cliCommandManager = new CLICommandManager();
+
+	public MCSkyBot(@NotNull File dataFolder) {
+		this.dataFolder = dataFolder;
+		this.cfg = new YamlFile(dataFolder, "config.yml");
+	}
+
+	@NotNull
+	public YamlFile getConfig() {
+		return cfg;
+	}
+
+	protected abstract void onStart();
+
+	public final void start() {
+		setupCLI();
+		setupConfig();
+		onStart();
+	}
+
+	private void setupCLI() {
+		cliCommandManager.start();
+		cliCommandManager.registerConsumer("stop", args -> {
+			cliCommandManager.stop();
+			onStop(args);
+			afterStop();
+		});
+	}
+
+	private void setupConfig() {
+		cfg.setup(err -> {
+			System.err.println("Failed to setup config file - " + err.getMessage() + ":");
+			err.printStackTrace(System.err);
+			ExitCode.CONFIG_SETUP_FAIL.exit();
+		});
+	}
+
+	protected abstract void onStop(@NotNull String @NotNull [] args);
+
+	private void afterStop() {
+		ExitCode.OK.exit();
 	}
 }
