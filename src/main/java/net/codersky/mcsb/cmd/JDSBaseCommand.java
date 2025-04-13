a0e239fa10bb4a9b010bded25a59863c5fbbcd6a
xDec0de_ <xdec0deyt@gmail.com>
2025-04-13 19:26:48 +0200

    Added JDSBaseCommand & JDSICommand
    This lets us differentiate between base commands (That can't be executed) and subcommands. Implementing the onSlashCommand method for base commands was useless, so this is the solution I came up with. Also, prevents executable subcommands to have subcommands registered to them

---
+++ src/main/java/net/codersky/mcsb/cmd/JDSBaseCommand.java
@@ -0,0 +1,48 @@
+package net.codersky.mcsb.cmd;
+
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * {@link JDSICommand} implementation designed for base commands that
+ * won't be executed. This implementation only allows other
+ * {@link JDSCommand subcommands} to be added to it, acting as a main
+ * base command.
+ *
+ * @author xDec0de_
+ *
+ * @since JDSky 1.0.0
+ */
+public class JDSBaseCommand implements JDSICommand {
+
+	private final SlashCommandData data;
+
+	public JDSBaseCommand(@NotNull String name, @NotNull String desc) {
+		this.data = Commands.slash(name, desc);
+	}
+
+	@NotNull
+	@Override
+	public SlashCommandData getSlashCommandData() {
+		return data;
+	}
+
+	/**
+	 * Adds up to {@link CommandData#MAX_OPTIONS 25} {@link JDSCommand commands}
+	 * as subcommands of this {@link JDSBaseCommand}.
+	 *
+	 * @param subcommands The {@link JDSCommand subcommands} to add to this {@link JDSCommand}.
+	 *
+	 * @return This {@link JDSCommand}, the base command.
+	 */
+	public JDSBaseCommand addSubCommand(@NotNull JDSCommand<?>... subcommands) {
+		final SubcommandData[] data = new SubcommandData[subcommands.length];
+		for (int i = 0; i < subcommands.length; i++)
+			data[i] = SubcommandData.fromData(subcommands[i].getSlashCommandData().toData());
+		this.data.addSubcommands(data);
+		return this;
+	}
+}
---
+++ src/main/java/net/codersky/mcsb/cmd/JDSICommand.java
@@ -0,0 +1,178 @@
+package net.codersky.mcsb.cmd;
+
+import net.dv8tion.jda.api.interactions.DiscordLocale;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.interactions.commands.localization.LocalizationMap;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Interface for any type of command that holds {@link SlashCommandData},
+ * so implementations of Discord slash commands.
+ *
+ * @author xDec0de_
+ *
+ * @since JDSky 1.0.0
+ */
+public interface JDSICommand {
+
+	/**
+	 * Gets the {@link SlashCommandData} being used internally
+	 * by this {@link JDSICommand}.
+	 *
+	 * @return The {@link SlashCommandData} being used internally
+	 * by this {@link JDSICommand}.
+	 *
+	 * @since JDSky 1.0.0
+	 */
+	@NotNull
+	SlashCommandData getSlashCommandData();
+
+	/*
+	 - Name
+	 */
+
+	/**
+	 * Gets the {@link LocalizationMap} for the <b>name</b> of this command. This map
+	 * allows having a different command name for all the {@link DiscordLocale languages}
+	 * supported by Discord.
+	 *
+	 * @return The <b>name</b> {@link LocalizationMap} of this {@link JDSICommand}.
+	 *
+	 * @since JDSky 1.0.0
+	 */
+	@NotNull
+	default LocalizationMap getNameLocalizations() {
+		return getSlashCommandData().getNameLocalizations();
+	}
+
+	/**
+	 * Sets the name of this {@link JDSICommand} on different {@link DiscordLocale languages}.
+	 * You can set different names for different locales, or one for each locale.
+	 *
+	 * @param name The name to set for the provided {@code locales}.
+	 * @param locales The locales that will use the provided {@code name}. You can use one
+	 * or as many as you want.
+	 *
+	 * @return This {@link JDSICommand}.
+	 *
+	 * @since JDSky 1.0.0
+	 */
+	@NotNull
+	default JDSICommand setName(@NotNull String name, @NotNull DiscordLocale @NotNull ... locales) {
+		final LocalizationMap map = getNameLocalizations();
+		for (DiscordLocale locale : locales)
+			map.setTranslation(locale, name);
+		return this;
+	}
+
+	/**
+	 * Sets the name of this {@link JDSICommand} for all {@link DiscordLocale languages}. This
+	 * will override any other {@link #setName(String, DiscordLocale...) setting} for any other
+	 * languages, so if you want to set a default, this must be done first.
+	 *
+	 * @param name The name of this {@link JDSICommand} for <b>all</b>
+	 * {@link DiscordLocale languages}.
+	 *
+	 * @return This {@link JDSICommand}.
+	 *
+	 * @since JDSky 1.0.0
+	 */
+	default JDSICommand setName(@NotNull String name) {
+		return setName(name, DiscordLocale.values());
+	}
+
+	/*
+	 - Description
+	 */
+
+	/**
+	 * Gets the {@link LocalizationMap} for the <b>description</b> of this command. This map
+	 * allows having a different command description for all the {@link DiscordLocale languages}
+	 * supported by Discord.
+	 *
+	 * @return The <b>description</b> {@link LocalizationMap} of this {@link JDSICommand}.
+	 *
+	 * @since JDSky 1.0.0
+	 */
+	@NotNull
+	default LocalizationMap getDescLocalizations() {
+		return getSlashCommandData().getDescriptionLocalizations();
+	}
+
+	/**
+	 * Sets the description of this {@link JDSICommand} on different {@link DiscordLocale languages}.
+	 * You can set different descriptions for different locales, or one for each locale.
+	 *
+	 * @param desc The description to set for the provided {@code locales}.
+	 * @param locales The locales that will use the provided {@code name}. You can use one
+	 * or as many as you want.
+	 *
+	 * @return This {@link JDSICommand}.
+	 *
+	 * @since JDSky 1.0.0
+	 */
+	@NotNull
+	default JDSICommand setDesc(@NotNull String desc, @NotNull DiscordLocale @NotNull ... locales) {
+		final LocalizationMap map = getDescLocalizations();
+		for (DiscordLocale locale : locales)
+			map.setTranslation(locale, desc);
+		return this;
+	}
+
+	/**
+	 * Sets the description of this {@link JDSICommand} for all {@link DiscordLocale languages}. This
+	 * will override any other {@link #setDesc(String, DiscordLocale...) setting} for any other
+	 * languages, so if you want to set a default, this must be done first.
+	 *
+	 * @param desc The description of this {@link JDSICommand} for <b>all</b>
+	 * {@link DiscordLocale languages}.
+	 *
+	 * @return This {@link JDSICommand}.
+	 *
+	 * @since JDSky 1.0.0
+	 */
+	default JDSICommand setDesc(@NotNull String desc) {
+		return setDesc(desc, DiscordLocale.values());
+	}
+
+	/*
+	 - NSFW
+	 */
+
+	/**
+	 * Whether this command should only be usable on age-restricted channels or not.
+	 * NSFW commands cannot be used on regular channels.
+	 *
+	 * @return The NSFW status of this {@link JDSICommand}. {@code true} if enabled
+	 * {@code false} otherwise.
+	 *
+	 * @since JDSky 1.0.0
+	 *
+	 * @see #setNSFW(boolean)
+	 * @see <a href="https://support.discord.com/hc/en-us/articles/10123937946007">
+	 *     Age-Restricted Commands FAQ</a>
+	 */
+	default boolean isNSFW() {
+		return getSlashCommandData().isNSFW();
+	}
+
+	/**
+	 * Sets the NSFW status of this command. NSFW commands cannot be used on regular channels,
+	 * only on age-restricted channels.
+	 *
+	 * @param nsfw The new NSFW status of this {@link JDSICommand}.
+	 *
+	 * @return This {@link JDSICommand}.
+	 *
+	 * @since JDSky 1.0.0
+	 *
+	 * @see #isNSFW()
+	 * @see <a href="https://support.discord.com/hc/en-us/articles/10123937946007">
+	 *     Age-Restricted Commands FAQ</a>
+	 */
+	@NotNull
+	default JDSICommand setNSFW(boolean nsfw) {
+		getSlashCommandData().setNSFW(nsfw);
+		return this;
+	}
+}
