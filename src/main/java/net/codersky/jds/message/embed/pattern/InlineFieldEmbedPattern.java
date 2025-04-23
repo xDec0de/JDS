dbfb1554290fb3e9290919ebaa9431a44d93f366
xDec0de_ <xdec0deyt@gmail.com>
2025-04-24 01:48:25 +0200

    Added field & inline field embed patterns

---
+++ src/main/java/net/codersky/jds/message/embed/pattern/InlineFieldEmbedPattern.java
@@ -0,0 +1,22 @@
+package net.codersky.jds.message.embed.pattern;
+
+import net.codersky.jsky.annotations.KeyPattern;
+import net.codersky.jsky.collections.JCollections;
+import net.codersky.jsky.strings.tag.JTag;
+import net.dv8tion.jda.api.EmbedBuilder;
+import org.jetbrains.annotations.NotNull;
+
+public class InlineFieldEmbedPattern implements EmbedPattern {
+
+	@Override
+	public void apply(@NotNull EmbedBuilder embed, @NotNull String context, @NotNull JTag[] extra) {
+		final JTag value = JCollections.get(extra, tag -> tag.getName().equals("value") || tag.getName().equals("v"));
+		final String valueStr = value == null ? "" : value.getContent();
+		embed.addField(context, valueStr, true);
+	}
+
+	@Override
+	public @NotNull @KeyPattern String getKey() {
+		return "ifield";
+	}
+}
